92c5a6b5a852e8a0060c950b19d4fc9c97b627a7
Eduardo Gomes <85569960+eduardofgomes@users.noreply.github.com>
2021-09-02 17:34:02 +0000

    Correção de erros

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ var filesToCache = [
 ];
 self.addEventListener('install', function(e){
   e.waitUntil(
-    caches.open(cacheName).then(function(cacahe){
+    caches.open(cacheName).then(function(cache){
       return cache.addAll(filesToCache);
     })
   );
@@ -14,7 +14,7 @@ self.addEventListener('install', function(e){
 self.addEventListener('fetch', function(e){
   e.respondWith(
     caches.match(e.request).then(function(response){
-      return  response || fetch(e.request);
+      return response || fetch(e.request);
     })
   );
 });
